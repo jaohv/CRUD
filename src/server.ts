876ea9e6b1b70bebd7e7b85439f3ec16e1585411
jaohv <joao.rmiranda@icloud.com>
2023-04-14 14:15:55 -0300

    feat: get users done

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import "reflect-metadata";
 import express from "express";
 import cors from "cors";
 import { AppDataSource } from "./database/data-source";
+import routers from "./app/routes/Routes";
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json())
+
+app.use(routers);
 
 AppDataSource.initialize().then(async () => {
     console.log("Database OK")
