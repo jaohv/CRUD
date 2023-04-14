015bb1ce3bf12a139be58a54b1368bb591fb10ae
jaohv <joao.rmiranda@icloud.com>
2023-04-14 11:36:19 -0300

    add migration in project

---
+++ src/database/migrations/1681482963732-CreateUsersTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateUsersTable1681482963732 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
